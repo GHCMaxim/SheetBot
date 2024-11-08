f823c60d4064ed58f857a1eb04f838e4c46fdc0f
GHCMaxim <maxim080503@gmail.com>
2024-11-08 23:40:16 +0700

    feat: added offsync, show quotes

--- src/sheetbot/bot.py
+++ src/sheetbot/bot.py
@@ -1,6 +1,8 @@
+import asyncio
 import os
 import discord
 from discord.ext import commands
+from discord.ui import Button, View, button
 from dotenv import load_dotenv
 import random
 from sheetCommands import (
@@ -21,7 +23,6 @@ used_symbol = "s>"
 
 spreadSrv, driveSrv = get_service()
 
-print(get_sheet_values(spreadSrv, SHEET_ID, "in sync!A12:C12"))
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -61,6 +62,23 @@ async def help(ctx):
     embed.add_field(
         name="add_quote <quote>", value="Adds a quote to the list of quotes", inline=False
     )
+    embed.add_field(
+        name="read_offsync <number>", value="Reads the off sync entry at the given number", inline=False
+    )
+    embed.add_field(
+        name="read_max_offsync", value="Reads the current max off sync entry number", inline=False
+    )
+    embed.add_field(
+        name="write_offsync <message>",
+        value="Writes the message to the off sync sheet. Please put the message in quotes.",
+        inline=False,
+    )
+    embed.add_field(
+        name="help", value="Displays this message", inline=False
+    )
+    embed.add_field(
+        name="show_quotes", value="Displays a list of quotes", inline=False
+    )
     embed.set_footer(text=chosen_line)
     await ctx.send(embed=embed)
 
@@ -73,7 +91,24 @@ async def ping(ctx):
 @bot.command()
 async def read_max(ctx):
     value = get_sheet_values(spreadSrv, SHEET_ID, "in sync!E3:E3")
-    await ctx.send(f"Same count is currently at: {value["values"][0][0]}")
+    embed = discord.Embed(
+        title="Same!",
+        description=f"Same! count: {value['values'][0][0]}",
+        color=int("0x2ECC71", 16),
+    )
+    embed.set_footer(text=quotes[random.randint(0, len(quotes) - 1)])
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def read_max_offsync(ctx):
+    value = get_sheet_values(spreadSrv, SHEET_ID, "off sync!E3:E3")
+    embed = discord.Embed(
+        title="Off sync!",
+        description=f"Off sync! count: {value['values'][0][0]}",
+        color=int("0x2ECC71", 16),
+    )
+    embed.set_footer(text=quotes[random.randint(0, len(quotes) - 1)])
+    await ctx.send(embed=embed)
 
 
 @bot.command()
@@ -83,6 +118,7 @@ async def read(ctx, num):
         num = int(num)
         value = get_sheet_values(spreadSrv, SHEET_ID, f"in sync!C{num+2}")
         string_value = value["values"][0]
+        print(value["values"])
         if string_value == []:
             await ctx.send("No entry found")
             return
@@ -98,6 +134,27 @@ async def read(ctx, num):
         await ctx.send("Please provide a valid number")
         return
 
+@bot.command()
+async def read_offsync(ctx, num):
+    try:
+        chosen_line = quotes[random.randint(0, len(quotes) - 1)]
+        num = int(num)
+        value = get_sheet_values(spreadSrv, SHEET_ID, f"off sync!C{num+2}")
+        string_value = value["values"][0]
+        if string_value == []:
+            await ctx.send("No entry found")
+            return
+        string_value = [x.replace("\\n", "\n") for x in string_value]
+        embed = discord.Embed(
+            title=f"Entry {num}",
+            description=f"{string_value[0]}",
+            color=int("0x2ECC71", 16),
+        )
+        embed.set_footer(text=chosen_line)
+        await ctx.send(embed=embed)
+    except ValueError:
+        await ctx.send("Please provide a valid number")
+        return
 
 @bot.command()
 async def write(ctx, arg):
@@ -119,7 +176,41 @@ async def write(ctx, arg):
     except Exception as e:
         await ctx.send(f"Failed to write to sheet: {e}")
         return
-    await ctx.send(f"Successfully wrote entry {current+1} ")
+    embed = discord.Embed(
+        title=f"Successfully wrote entry {current+1}",
+        description=f"{arg}",
+        color=int("0x2ECC71", 16),
+    )
+    embed.set_footer(text=quotes[random.randint(0, len(quotes) - 1)])
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def write_offsync(ctx, arg):
+    if arg[0] == '"':
+        end = arg.find('"', 1)
+        if end == -1:
+            await ctx.send("Invalid format")
+            return
+        arg = arg[1:end]
+    current = get_sheet_values(spreadSrv, SHEET_ID, "off sync!E3:E3")
+    current = int(current["values"][0][0])
+    try:
+        write_to_sheet(
+            spreadSrv,
+            SHEET_ID,
+            f"off sync!A{current+3}:C{current+3}",
+            {"values": [[current + 1, "", arg]]},
+        )
+    except Exception as e:
+        await ctx.send(f"Failed to write to sheet: {e}")
+        return
+    embed = discord.Embed(
+        title=f"Successfully wrote entry {current+1}",
+        description=f"{arg}",
+        color=int("0x2ECC71", 16),
+    )
+    embed.set_footer(text=quotes[random.randint(0, len(quotes) - 1)])
+    await ctx.send(embed=embed)
 
 
 @bot.command()
@@ -128,7 +219,13 @@ async def add_wording(ctx):
     current = int(current["values"][0][0])
     try:
         write_to_sheet(spreadSrv, SHEET_ID, "in sync!E4", {"values": [[current + 1]]})
-        await ctx.send(f"Current count: {current+1}")
+        embed = discord.Embed(
+            title="Wording!",
+            description=f"Current count: {current+1}",
+            color=int("0x2ECC71", 16),
+        )
+        embed.set_footer(text=quotes[random.randint(0, len(quotes) - 1)])
+        await ctx.send(embed=embed)
         return
     except Exception as e:
         await ctx.send(f"Failed to write to sheet: {e}")
@@ -139,14 +236,62 @@ async def add_wording(ctx):
 async def current_wording(ctx):
     current = get_sheet_values(spreadSrv, SHEET_ID, "in sync!E4")
     current = int(current["values"][0][0])
-    await ctx.send(f"Current Wording! count: {current}")
+    embed = discord.Embed(
+        title="Wording!",
+        description=f"Current count: {current}",
+        color=int("0x2ECC71", 16),
+    )
+    embed.set_footer(text=quotes[random.randint(0, len(quotes) - 1)])
+    await ctx.send(embed=embed)
     return
 
 @bot.command()
 async def add_quote(ctx, quote):
     with open("quotes.txt", "a") as f:
         f.write(f"\n{quote}")
-    await ctx.send("Quote added!")
     quotes.append(quote)
+    embed = discord.Embed(
+        title="Quote added!",
+        description=f"{quote}",
+        color=int("0x2ECC71", 16),
+    )
+    embed.set_footer(text=quotes[random.randint(0, len(quotes) - 1)])
+    await ctx.send(embed=embed)
+    return
+
+
+class Quotes(View):
+    # purpose: Create an embed with a list of 10 quotes with pagination, and 2 buttons to navigate through the pages
+    def __init__(self, quotes):
+        super().__init__()
+        self.quotes = quotes
+        self.current_page = 0
+        self.embed = discord.Embed(
+            title="Quotes",
+            description="\n".join(quotes[self.current_page : self.current_page + 10]),
+            color=int("0x2ECC71", 16),
+        )
+        self.embed.set_footer(text=f"Page {self.current_page//10 + 1}")
+
+    @button(label="Previous", style=discord.ButtonStyle.primary)
+    async def prev(self,interaction, button ):
+        self.current_page -= 10
+        self.embed.description = "\n".join(self.quotes[self.current_page : self.current_page + 10])
+        self.embed.set_footer(text=f"Page {self.current_page//10 + 1}")
+        await interaction.response.edit_message(embed=self.embed, view=self)
+    
+    @button(label="Next", style=discord.ButtonStyle.primary)
+    async def next(self,interaction, button ):
+        self.current_page += 10
+        self.embed.description = "\n".join(self.quotes[self.current_page : self.current_page + 10])
+        self.embed.set_footer(text=f"Page {self.current_page//10 + 1}")
+        await interaction.response.edit_message(embed=self.embed, view=self)
+    
+
+@bot.command()
+async def show_quotes(ctx):
+    view = Quotes(quotes)
+    await ctx.send(embed=view.embed, view=view)
+
 
 bot.run(TOKEN)
